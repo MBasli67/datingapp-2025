30c3a9bf8ac6711c943069c5ff998ebf470c16fb
BASMIT <m.basli@ctisystems.com>
2025-11-18 11:39:26 +0100

    End of section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,10 @@
+using System.Text;
 using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +24,36 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 builder.Services.AddCors();
 //#####################################
 
+//####################################
+//Register the token service with token service interface Three different lifetimes
+/*
+1 --> Singleton
+2 --> Transient
+3 --> Scoped
+*/
+builder.Services.AddScoped<ITokenService, TokenService>();
+//####################################
+
+//#############################################
+/*
+ --> 1 --> Add authentication as as service
+ --> 2 --> Add middleware
+
+/
+*/
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    var tokenKey = builder.Configuration["TokenKey"] ?? throw new Exception("Token key not found - Program.cs");
+
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+//#############################################
 
 var app = builder.Build();
 
@@ -30,7 +65,13 @@ app.UseCors(x =>
  AllowAnyMethod().
  WithOrigins("http://localhost:4200", "https://localhost:4200")
  );
+
 //#######################################
+//Add authentication middleware
+//#######################################
+app.UseAuthentication(); //Who are you?
+app.UseAuthorization(); //Are you allowed?
+//##############################
 
 
 app.MapControllers();
