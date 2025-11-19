49adac049849877b0f4585eef5abc4a2fd458bfe
BASMIT <m.basli@ctisystems.com>
2025-11-19 18:56:04 +0100

    End of section 5

---
+++ client/src/layout/nav/nav.ts
@@ -0,0 +1,34 @@
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { AccountService } from '../../core/services/account-service';
+
+@Component({
+  selector: 'app-nav',
+  imports: [FormsModule],
+  templateUrl: './nav.html',
+  styleUrl: './nav.css',
+})
+export class Nav {
+
+  protected accountService = inject(AccountService);
+  protected creds: any = {};
+
+  login()
+  {
+          this.accountService.login(this.creds).
+          subscribe(
+            {
+            next: result => {
+              console.log(result),
+              this.creds = {}
+            },
+            error: error => alert(error),
+            }
+        );
+  }
+
+  logOut(){
+    this.accountService.logout();
+  }
+
+}
